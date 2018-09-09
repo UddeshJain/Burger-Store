4bbea4894e79ff8c9ac823ef502584b011c5b90e
UddeshJain <jainuddesh35@gmail.com>
2018-09-09 23:54:44 +0530

    Side drawer working partially

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../UI/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../UI/Navigation/SideDrawer/sideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-); 
+class Layout extends Component{
 
-export default layout;
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+    render() {
+        return(
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+} 
+
+export default Layout;
