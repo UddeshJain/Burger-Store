4180b8bb863101daf5d5d07fc1fa1c3a045d2b0f
UddeshJain <jainuddesh35@gmail.com>
2018-10-05 12:38:51 +0530

    Testing is left

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,6 @@ import classes from './Burger.css';
 import BurgerIngrediant from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map( (_, i) => {
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = ( orderData, token ) => {
         dispatch( purchaseBurgerStart() );
         axios.post( '/orders.json?auth=' + token, orderData )
             .then( response => {
-                console.log( response.data );
                 dispatch( purchaseBurgerSuccess( response.data.name, orderData ) );
             } )
             .catch( error => {
@@ -62,10 +61,11 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
-        axios.get( '/orders.json?auth=' + token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get( '/orders.json' + queryParams)
             .then( res => {
                 const fetchedOrders = [];
                 for ( let key in res.data ) {
